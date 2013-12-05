8cb680064f47f7f550a1673961ad0ac9f7590636
Fabian <fthielisch@gmail.com>
2013-12-04 22:47:29 -0500

    Skeleton maven code setup

---
+++ README.md
@@ -0,0 +1,9 @@
+TEMPLATE
+========
+
+This is a description, right?
+
+Compiling
+=========
+
+Just use maven.
---
+++ src/main/java/com/gmail/fthielisch/tenjava/Plugin.java
@@ -0,0 +1,8 @@
+
+
+public class Plugin {
+    
+    public static void main(String[] args) {
+        System.out.println("Hi!");
+    }
+}
